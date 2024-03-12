9046b1f6276d369fdc08792310cffc82dba2abf0
polgrisha <polyakov.ga@phystech.edu>
2024-03-12 12:14:27 +0300

    change token

--- get_token.py
+++ get_token.py
@@ -13,8 +13,8 @@ headers = {
 'RqUID': '6f0b1291-c7f3-43c6-bb2e-9f3efb2dc98e',
 'Authorization': 'Basic MDI2ODVhZjMtYWNmYi00NDE2LWFiOWMtYjNlNjY5OTVlOWRlOmRiYjQwZTM1LWMzMDQtNDI4Yi1iNGQyLTUyMTUwNjYwNThmZQ=='
 }
-
 response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+print(response)
 json.dump(json.loads(response.text), open('token.txt', 'w'))
     
     
