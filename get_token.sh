dc968cf782dcde7066feebe84294bfe657863ad5
polgrisha <polyakov.ga@phystech.edu>
2024-03-12 12:12:15 +0300

    change token

--- get_token.sh
+++ get_token.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-python get_token.py
-git commit -am "change token"
-git push
+while true
+    do
+        python get_token.py
+        git commit -am "change token"
+        git push
+    done
